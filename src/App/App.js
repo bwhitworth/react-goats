41b1cba75ac7f090122a4d22bba932a57d8f123b
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-05-11 18:56:29 -0500

    updated goat Data

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.scss';
+import goatData from '../helpers/data/goatData';
 
 class App extends React.Component {
+  // outside render - anything that modifies state
+  // anything that changes the data
+  componentDidMount() {
+    const goats = goatData.getGoats();
+    console.error('goats:', goats);
+  }
+
   render() {
+    // inside render - anything we need to modify the UI
+    // only displaying something with the data that is available from ^
     return (
       <div className="App">
         <h2>INSIDE APP COMPONENT</h2>
